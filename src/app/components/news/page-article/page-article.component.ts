272751e9edf0ea70bf4b292864f9d1bbd2562af9
Adam Hlaváček <git@adamhlavacek.com>
2021-10-11 11:24:41 +0200

    feat(news): show article content

--- src/app/components/news/page-article/page-article.component.ts
+++ src/app/components/news/page-article/page-article.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { BackendService, KsiTitleService } from "../../../services";
+import { ActivatedRoute } from "@angular/router";
+import { map, mergeMap, tap } from 'rxjs/operators';
+import { ArticleResponse } from "../../../../api";
+import { Observable } from "rxjs";
+import { Utils } from "../../../util";
 
 @Component({
   selector: 'ksi-page-article',
@@ -8,10 +14,19 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 })
 export class PageArticleComponent implements OnInit {
 
+  articleResponse$: Observable<ArticleResponse>;
 
-  constructor() { }
+  constructor(private backend: BackendService, private route: ActivatedRoute, private title: KsiTitleService) { }
 
   ngOnInit(): void {
+    this.articleResponse$ = this.route.params.pipe(
+      mergeMap((params) => this.backend.http.articlesGetSingle(Number(params.id))),
+      map((response) =>
+        ({...response, article: {...response.article, picture: Utils.parseLegacyAssetsUrl(response.article.picture)}})
+      ),
+      tap((response) => {
+        this.title.subtitle = response.article.title;
+      })
+    );
   }
-
 }

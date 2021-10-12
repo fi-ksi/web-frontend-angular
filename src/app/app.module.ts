e6487c7b27fbe45f4678f0fef3ab37ebdc171e97
Adam Hlaváček <git@adamhlavacek.com>
2021-10-12 22:18:28 +0200

    chore(root): move into separate module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,25 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './components/root/navbar/navbar.component';
 import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
-import { FooterComponent } from './components/root/footer/footer.component';
 import { AchievementService, BackendService, ThemeService, VersionService, YearsService } from './services';
 import { WindowService } from './services';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { KsiTitleService } from "./services";
-import { PageWelcomeComponent } from './components/root/page-welcome/page-welcome.component';
-import { PageNotFoundComponent } from './components/root/page-not-found/page-not-found.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { CarouselModule } from "ngx-bootstrap/carousel";
-import { CollapseModule } from "ngx-bootstrap/collapse";
-import { ModalChangelogComponent } from './components/root/modal-changelog/modal-changelog.component';
 import { BsModalService } from "ngx-bootstrap/modal";
-import { ReactiveFormsModule } from "@angular/forms";
-import { SharedModule } from "./components/shared/shared.module";
-import { SpinnerComponent } from './components/shared/spinner/spinner.component';
+import { RootModule } from "./components/root/root.module";
 
 export function HttpLoaderFactory(http: HttpClient): TranslateHttpLoader {
   return new TranslateHttpLoader(http);
@@ -30,11 +21,6 @@ export function HttpLoaderFactory(http: HttpClient): TranslateHttpLoader {
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent,
-    FooterComponent,
-    PageWelcomeComponent,
-    PageNotFoundComponent,
-    ModalChangelogComponent,
   ],
   imports: [
     BrowserModule,
@@ -50,10 +36,7 @@ export function HttpLoaderFactory(http: HttpClient): TranslateHttpLoader {
     }),
     NgbModule,
     BrowserAnimationsModule,
-    CarouselModule,
-    CollapseModule,
-    ReactiveFormsModule,
-    SharedModule
+    RootModule
   ],
   exports: [
     TranslateModule,
---
+++ src/app/components/root/root.module.ts
@@ -0,0 +1,44 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FooterComponent } from "./footer/footer.component";
+import { ModalChangelogComponent } from "./modal-changelog/modal-changelog.component";
+import { NavbarComponent } from "./navbar/navbar.component";
+import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
+import { PageWelcomeComponent } from "./page-welcome/page-welcome.component";
+import { SharedModule } from "../shared/shared.module";
+import { TranslateModule } from "@ngx-translate/core";
+import { CarouselModule } from "ngx-bootstrap/carousel";
+import { CollapseModule } from "ngx-bootstrap/collapse";
+import { ReactiveFormsModule } from "@angular/forms";
+import { RouterModule } from "@angular/router";
+import { NgbDropdownModule } from "@ng-bootstrap/ng-bootstrap";
+
+
+
+@NgModule({
+  declarations: [
+    FooterComponent,
+    ModalChangelogComponent,
+    NavbarComponent,
+    PageNotFoundComponent,
+    PageWelcomeComponent
+  ],
+  exports: [
+    FooterComponent,
+    ModalChangelogComponent,
+    NavbarComponent,
+    PageNotFoundComponent,
+    PageWelcomeComponent
+  ],
+  imports: [
+    CommonModule,
+    SharedModule,
+    TranslateModule,
+    CarouselModule,
+    CollapseModule,
+    ReactiveFormsModule,
+    RouterModule,
+    NgbDropdownModule,
+  ]
+})
+export class RootModule { }

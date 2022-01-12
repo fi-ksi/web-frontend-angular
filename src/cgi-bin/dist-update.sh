c1ecdae9c1c66aa8d1c1709790f25773f7ebc741
Adam Hlaváček <git@adamhlavacek.com>
2022-01-12 12:50:59 +0100

    feat(autodeploy): notify deploy script

--- src/cgi-bin/dist-update.sh
+++ src/cgi-bin/dist-update.sh
@@ -5,8 +5,11 @@ GITHUB_OWNER="esoadamo"
 GITHUB_REPOSITORY="web-frontend-angular"
 PATCH_FILE_NAME="build.tar"
 
-TARGET_DIR="$(pwd)"
+TARGET_DIR="$(dirname "$(dirname "$(realpath "$0")")")"
 INPUT="$(cat)"
+
+[ -z "$HOME" ] && HOME="$(eval echo "~$(whoami)")"
+echo "Deploying as $(whoami), TARGET=$TARGET_DIR, HOME=$HOME" >&2
 
 function main() {
   downloadUrl="$INPUT"
@@ -21,7 +24,7 @@ function main() {
     # continues only if github owner and repo and filename matches
     downloadPath="$(mktemp --suffix=.tar)" &&
     extractDir="$(mktemp -d)" &&
-    wget --output-document "$downloadPath" "$downloadUrl" > /dev/null &&
+    wget --progress=bar:force:noscroll --output-document "$downloadPath" "$downloadUrl" > /dev/null &&
     tar -xf "$downloadPath" -C "$extractDir" > /dev/null &&
     rm "$downloadPath" > /dev/null &&
     rsync -r --delete-after "$extractDir/" "$TARGET_DIR" > /dev/null &&
@@ -52,17 +55,19 @@ function __run_main_sandboxed() {
     if ps -p 1 | tail -n 1 | grep --quiet -Fv 'firejail'; then # firejail is not active right now
       firejail --help &>/dev/null &&  # firejail is available
       echo "Initializing sandboxed process" >&2
-      echo "$INPUT" | firejail --noprofile \
+      echo "$INPUT" | firejail \
+               --noprofile \
                --private-tmp \
+               --read-write="$HOME" \
                --private="$TARGET_DIR" \
                --protocol=inet,inet6 \
                --noautopulse \
                --novideo \
                --nosound \
                --nodvd \
-               --private-cwd="$TARGET_DIR" \
+               --shell=/bin/bash \
                -- \
-               "./$(basename "$0")"
+               bash "$(basename "$(dirname "$(realpath "$0")")")/$(basename "$0")"
       true
     else
       echo "Running sandboxed" >&2

1de480ae9a8d991d0b8cfbfa06d036d9130e8816
Adam Hlaváček <git@adamhlavacek.com>
2022-01-21 19:01:48 +0100

    feat(tasks): sortable module drags on desktop

--- src/app/components/tasks/task-module/task-module-sortable/task-module-sortable.component.ts
+++ src/app/components/tasks/task-module/task-module-sortable/task-module-sortable.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit, ChangeDetectionStrategy, Input, ChangeDetectorRef } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  ChangeDetectionStrategy,
+  Input,
+  ChangeDetectorRef,
+  HostListener, ViewChild, ElementRef
+} from '@angular/core';
 import { ModuleSortable } from "../../../../../api";
 import { ModuleService, WindowService } from "../../../../services";
 import { ISortableItem } from "../../../../models";
@@ -13,6 +20,44 @@ import { Observable } from "rxjs";
 export class TaskModuleSortableComponent implements OnInit {
   @Input()
   module: ModuleSortable;
+
+  @ViewChild('draggedItem', { static: false })
+  private selectedItemEl?: ElementRef<HTMLElement>;
+  private lastClickPos?: {x: number, y: number};
+  private mouseMoveTimer?: number;
+
+  @HostListener('mousemove', ['$event'])
+  onMouseMove(event: MouseEvent): void {
+    if (this.mouseMoveTimer !== undefined) {
+      return;
+    }
+    // @ts-ignore
+    this.mouseMoveTimer = setTimeout(() => {
+      if (this.selectedItemEl
+        && (
+          // if the last click was on the same place as move, ignore this move
+          // this is a typical situation on touchscreen devices
+          !this.lastClickPos ||
+          this.lastClickPos.x !== event.x ||
+          this.lastClickPos.y !== event.y
+        )
+      ) {
+        const el = this.selectedItemEl.nativeElement;
+        const {style} = el;
+        const rec = el.getBoundingClientRect();
+        style.visibility = 'visible';
+        style.position = 'fixed';
+        style.top = `${event.y + (rec.height / 2)}px`;
+        style.left = `${event.x - (rec.width / 2)}px`;
+      }
+      this.mouseMoveTimer = undefined;
+    }, 1000 / 60) as number; // 60 FPS
+  }
+
+  @HostListener('click', ['$event'])
+  onMouseClick(event: MouseEvent): void {
+    this.lastClickPos = event;
+  }
 
   listLeft: ISortableItem[];
   listRight: ISortableItem[];

6e7cf5c40e926bfdee389200bb5f7b0d3ae31042
Adam Hlaváček <git@adamhlavacek.com>
2022-05-30 11:07:43 +0200

    patch(environment): log only errors on production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -7,9 +7,9 @@ export const environment: Environment = {
   backend: 'https://rest.naskoc.fi.muni.cz/',
   urlPrefix: '/',
   logger: {
-    log: console.log,
+    log: doNothing,
     error: console.error,
-    debug: console.debug,
-    warn: console.warn
+    debug: doNothing,
+    warn: doNothing
   }
 };

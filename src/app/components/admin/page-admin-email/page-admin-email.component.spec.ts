2bd44b317aad1e3f387cb8a4f0eeb7b30bbe7a3f
Adam Hlaváček <git@adamhlavacek.com>
2022-08-19 21:11:15 +0200

    feat(admin): create email component

---
+++ src/app/components/admin/page-admin-email/page-admin-email.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PageAdminEmailComponent } from './page-admin-email.component';
+
+describe('PageAdminEmailComponent', () => {
+  let component: PageAdminEmailComponent;
+  let fixture: ComponentFixture<PageAdminEmailComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ PageAdminEmailComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(PageAdminEmailComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/components/admin/page-admin-email/page-admin-email.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+
+@Component({
+  selector: 'ksi-page-admin-email',
+  templateUrl: './page-admin-email.component.html',
+  styleUrls: ['./page-admin-email.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class PageAdminEmailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

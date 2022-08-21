94575906a4ef71327e249927deafd030e83c2ce7
Adam Hlaváček <git@adamhlavacek.com>
2022-08-21 10:31:06 +0200

    feat(admin): email form

--- src/app/components/admin/page-admin-email/page-admin-email.component.ts
+++ src/app/components/admin/page-admin-email/page-admin-email.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, Validators } from '@angular/forms';
 import { take } from 'rxjs/operators';
+import { YearsService } from '../../../services';
 
 @Component({
   selector: 'ksi-page-admin-email',
@@ -11,19 +12,30 @@ import { take } from 'rxjs/operators';
 })
 export class PageAdminEmailComponent implements OnInit {
   emailForm = this.fb.group({
-    subject: [''],
-    body: ['']
+    to: [0, Validators.required],
+    subject: ['', Validators.required],
+    body: ['', Validators.required],
+    successful: [false, Validators.required],
+    type: [null, Validators.required],
+    sex: ['both', Validators.required],
   });
 
-  constructor(private translate: TranslateService, private fb: FormBuilder) { }
+  constructor(
+    private translate: TranslateService,
+    private fb: FormBuilder,
+    public years: YearsService
+  ) { }
 
   ngOnInit(): void {
     this.translate.get('admin.email.subject.initial').pipe(take(1))
       .subscribe((text) => {
         if (!this.emailForm.controls.subject.value) {
           this.emailForm.controls.subject.setValue(text);
+          this.emailForm.controls.subject.addValidators((x) => x.value === text ? {error: 'Cannot leave the default value'} : null);
+          this.emailForm.controls.subject.updateValueAndValidity();
         }
       });
-  }
 
+    this.years.selected$.pipe(take(1)).subscribe((v) => this.emailForm.controls.to.setValue(v?.id));
+  }
 }

7270569011d193e3b1801fb2ac1080d328fabdb0
Daniel Rozehnal <514184@mail.muni.cz>
2022-10-14 23:41:49 +0200

    fix: date timezone use date pipe

--- src/app/pipes/shared/ksi-date.pipe.ts
+++ src/app/pipes/shared/ksi-date.pipe.ts
@@ -1,15 +1,16 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import {DatePipe} from '@angular/common';
 
 @Pipe({
   name: 'ksiDate'
 })
 export class KsiDatePipe implements PipeTransform {
 
-  transform(value: string, ...args: unknown[]): string {
+  transform(value: string, ...args: any[]): string | null {
     const valueDate = new Date(value);
     const offsetInHours = - valueDate.getTimezoneOffset() / 60;
     const correctTime = valueDate.setHours(valueDate.getHours() + offsetInHours);
-    return new Date(correctTime).toLocaleString();
+    return new DatePipe('cs_CZ').transform(correctTime, ...args);
   }
 
 }

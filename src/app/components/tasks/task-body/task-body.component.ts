3d9aa77f39655c38953a160f1e2942677c84420a
jijikhal <jiji.h@seznam.cz>
2025-10-06 22:38:24 +0200

    fix: empty lines now contain something to be rendered

--- src/app/components/tasks/task-body/task-body.component.ts
+++ src/app/components/tasks/task-body/task-body.component.ts
@@ -33,7 +33,10 @@ highlight.addPlugin({
           return match;
         });
 
-        return `<div>${openTags.join('')}${line}${'</span>'.repeat(
+        // Ensure empty lines render visibly
+        const visibleLine = line.trim() === '' ? '&nbsp;' : line;
+
+        return `<div>${openTags.join('')}${visibleLine}${'</span>'.repeat(
           openTags.length
         )}</div>`;
       })

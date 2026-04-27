349a54c9c7578540293817216c3c28019fb9fe2b
Ninjaondra321 <ondran321@gmail.com>
2026-04-27 10:20:16 +0200

    Added link from WIP admin page to old admin

--- src/app/components/admin/page-admin-grading/page-admin-grading.component.ts
+++ src/app/components/admin/page-admin-grading/page-admin-grading.component.ts
@@ -3,6 +3,7 @@ import { AdminBaseComponent } from '../base/admin-base.component';
 import { IconService } from 'src/app/services/shared/icon.service';
 import { RoutesService } from 'src/app/services/shared/routes.service';
 import { ModalService } from 'src/app/services/shared/modal.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'ksi-page-admin-grading',
@@ -13,18 +14,26 @@ import { ModalService } from 'src/app/services/shared/modal.service';
 
 export class PageAdminGradingComponent extends AdminBaseComponent<void> { // TODO! Change from VOID
 
+  oldFrontendUrl = environment.oldFrontendUrl;
+
+
   constructor(
     public icon: IconService,
     public routes: RoutesService,
-    
+
     protected cdr: ChangeDetectorRef,
     protected modal: ModalService,
-    
+
   ) {
     super(modal, cdr);
   }
 
   ngOnInit(): void {
+  }
+
+  openExternal(event: MouseEvent): void {
+    const anchor = event.target as HTMLAnchorElement;
+    window.open(anchor.href, '_blank');
   }
 
 
